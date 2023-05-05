5808ed95d758f97818d45273abb879b5bfa751ed
EinYakAmNil <einyak@gmail.com>
2023-05-05 12:17:34 +0200

    fix syntax

--- sync-config.sh
+++ sync-config.sh
@@ -6,16 +6,16 @@ esc_DIR=$(echo $DIR | sed 's/\//\\\//g')
 sudo pacman -S - < $DIR/packages
 
 # Find all config files
-CONFIGS=$(find $DIR -type f -not \( -path "$DIR/\.git/*" -o -path "$DIR/git-repos" -o -path "$DIR/sync-config.sh" -o -path "$DIR/packages"\))
+CONFIGS=$(find $DIR -type f -not \( -path "$DIR/\.git/*" -o -path "$DIR/git-repos" -o -path "$DIR/sync-config.sh" -o -path "$DIR/packages" \))
 
 # Create the directories to which they belong
-CONFIG_DIRS=$(find $DIR -type d -not \( -path "$DIR/\.git" -o -path "$DIR/\.git/*" \) | sed "s/^$esc_DIR//" | sed "s/^\/home\/user/\/home\/xinux/")
+CONFIG_DIRS=$(find $DIR -type d -not \( -path "$DIR/\.git" -o -path "$DIR/\.git/*" \) | sed "s/^$esc_DIR//" | sed "s/^\/home\/user/\/home\/$USER/")
 sudo mkdir -v -p $CONFIG_DIRS
 
 # Copy them over
 for i in $CONFIGS
 do
-	sudo cp -iv "$i" $(echo $i | sed "s/^$esc_DIR//" | sed "s/^\/home\/user/\/home\/xinux/")
+	sudo cp -iv "$i" $(echo $i | sed "s/^$esc_DIR//" | sed "s/^\/home\/user/\/home\/$USER/")
 done
 
 # Set correct permissions for user configs
