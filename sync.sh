149a8d3eb9136fde0fc47368da5f53837e92adf5
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2025-01-06 14:56:55 +0100

    update synch script

--- sync.sh
+++ sync.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-ls ./sync-config.sh || echo "Please run this script in the directory of the repository" && exit 1
+[[ -z $(ls ./sync.sh) ]] && echo "Please run this script in the directory of the repository." && exit 1
 
-dotfiles_dir=$PWD
-
-# Download AUR helper in another directory
+echo "Installing AUR helper"
 sudo pacman -S git base-devel
-mkdir -pv $HOME/Repositorien
-cd $HOME/Repositorien
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-# Return to dofiles directory and proceed with configuration
-cd $dotfiles_dir
-
+echo "Installing packages"
 yay -S - < ./packages
 
-# Create directories for system level configuration
+echo "Create directories for system level configuration."
 find ./system/ -mindepth 1 -type d -printf '/%P\n' | while read dir
 do
 	sudo mkdir -pv $dir
 done
 
-# Create directories for user level configuration
+echo "Create directories for user level configuration."
 find ./user/ -mindepth 1 -type d -printf "$HOME/%P\n" | while read dir
 do
 	mkdir -pv $dir
