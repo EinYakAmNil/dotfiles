1046a87c44bb12073ffa7481548b05e933f334d3
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2025-04-12 13:17:29 +0200

    Anpassungen für neue awesome Konfiguration

--- user/.config/mpv/scripts/awesome-widget.lua
+++ user/.config/mpv/scripts/awesome-widget.lua
@@ -1,5 +1,6 @@
-function update_bar()
-	mp.commandv("run", "wimusic", "update")
+local function update_bar()
+	local update_script = os.getenv("XDG_CONFIG_HOME") .. "/awesome/scripts/music"
+	mp.commandv("run", update_script, "update")
 end
 
 mp.register_event("file-loaded", update_bar)
